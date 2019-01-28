5b90f7097f5654ac6692eeb70862823917a26d34
qarajesh2171 <raj_dixit1992@rediffmail.com>
2019-01-28 13:01:22 +0530

    Success

---
+++ Test Suites/New Test Suite 1.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b9eded16-bdc4-4e37-86a4-77282077f2c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>41bec672-3a08-4d97-8b4a-cb76d2176ff8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DDTest/New Test Case 1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>20a1bf66-40e3-47f3-8eb6-c5f6fbbf562b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data 1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>20a1bf66-40e3-47f3-8eb6-c5f6fbbf562b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Sendtext</value>
+         <variableId>dda32c84-6dd0-41ac-ab63-2bb4ea08ab26</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
